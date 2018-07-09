28711a5c29da3717b8069e3cd395d0599e4fb437
admin <admin@tplwkpcente-a8>
2018-07-09 16:57:29 +0200

    A

---
+++ src/main/java/com/example/agendaspring/AgendaspringApplication.java
@@ -0,0 +1,12 @@
+package com.example.agendaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgendaspringApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/agendaspring/AgendaspringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.agendaspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AgendaspringApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
